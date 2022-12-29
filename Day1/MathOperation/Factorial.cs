9687b7a40064e7043a013659b5967788a3283266
mayur <mayurpakhale30.com>
2022-12-29 16:39:05 +0530

    day1

---
+++ Day1/MathOperation/Factorial.cs
@@ -0,0 +1,27 @@
+using System;
+namespace  Operation;
+
+public class Factorial{
+
+    private int num;
+    public Factorial(){
+        this.num = 1;
+    }
+
+    public Factorial(int num){
+        this.num = num;
+    }
+
+    public int Facto(int num){
+        int i = num;
+        int res = 1;
+        while (i != 0)
+        {
+            res = res * i;
+            i--;
+        }
+        return res;
+    }
+
+
+}
---
+++ Day1/MathOperation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Operation;
+
+
+
+var p = new {
+            Program = "Factorial Program"
+};
+
+Console.WriteLine(p.Program);
+
+Factorial fact = new Factorial(20);
+int result = fact.Facto(5);
+Console.WriteLine("Factorial := "+result);
