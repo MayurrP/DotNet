805be54e0be12c17b826e9483d063d3e30a6353e
mayur <mayurpakhale30.com>
2022-12-29 17:50:43 +0530

    Upload

--- Day1/MathOperation/Calculator.cs
+++ Day1/MathOperation/Calculator.cs
@@ -1,6 +1,27 @@
 namespace Calculation;
 
 class Calculator{
-   
+
+    public Calculator(){}
+
+   public double Addition(double num1, double num2){
+    double res = num1 + num2;
+    return res;
+   }
+
+    public double Subtraction(double num1, double num2){
+    double res = num1 - num2;
+    return res;
+   }
+
+   public double Multiplication(double num1, double num2){
+    double res = num1 * num2;
+    return res;
+   }
+
+   public double Division(double num1, double num2){
+    double res = num1 / num2;
+    return res;
+   }
 
 }
--- Day1/MathOperation/Program.cs
+++ Day1/MathOperation/Program.cs
@@ -1,12 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Operation;
+using Calculation;
+
+double result;
 
 var p = new {
-            Program = "Factorial Program"
+            Program1 = "Factorial Program",
+            Program2 = "Caculator Programs"
 };
-
-Console.WriteLine(p.Program);
+Console.WriteLine("---------------------------------");
+Console.WriteLine(p.Program1);
 
 Factorial fact = new Factorial();
-int result = fact.Facto(5);
+result = fact.Facto(5);
 Console.WriteLine("Factorial := "+result);
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine("---------------------------------");
+Console.WriteLine(p.Program2);
+
+//------> Addition
+
+Calculator cal = new Calculator();
+
+result = cal.Addition(23.45,45.34);
+Console.WriteLine("Addition is : "+result);
+Console.WriteLine("---------------------------------");
+
+//------> Subtraction
+
+result = cal.Subtraction(56,35.34);
+Console.WriteLine("Subtraction is : "+result);
+Console.WriteLine("---------------------------------");
+
+//------> Multiplication
+
+result = cal.Multiplication(23,4);
+Console.WriteLine("Multiplication is : "+result);
+Console.WriteLine("---------------------------------");
+
+//------> Division
+
+result = cal.Division(20,4.5);
+Console.WriteLine("Division is : "+result);
+Console.WriteLine("---------------------------------");
